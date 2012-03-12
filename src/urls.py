cf0413acece3b52303661dbdf4b71397d720a85f
Christofer Bertonha <christoferbertonha@gmail.com>
2012-03-12 00:12:51 -0300

    Added a logo

--- src/urls.py
+++ src/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from core.views import homepage
 
@@ -18,3 +19,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
